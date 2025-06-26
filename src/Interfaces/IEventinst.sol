987a052f90393b21db838ae3b297cbf3494ce1c9
Sparsh <hitotsume.nozo@gmail.com>
2025-06-26 21:07:34 +0530

    Interface Files Addition

---
+++ src/Interfaces/IEventinst.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+interface IEventInstance {
+    function buy(string memory tier, uint256 quantity) external payable;
+    function cancel() external;
+    function payout() external;
+    function getTiers() external view returns (string[] memory);
+}
---
+++ src/Interfaces/ITicketNFT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+interface ITicketNFT {
+    function mintWithRandomness(address to, uint256 eventId, string memory uri_) external returns (uint256);
+    function uri(uint256 tokenId) external view returns (string memory);
+}
